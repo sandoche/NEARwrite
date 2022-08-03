1aaa7dad4f5c2dde24e6146674959d66c5982717
Sandoche Adittane <sandoche@gmail.com>
2022-08-03 13:26:25 +0200

    Basic contract wrote

--- contract/src/index.js
+++ contract/src/index.js
@@ -1,23 +1,28 @@
-import { NearContract, NearBindgen, call, view, near } from 'near-sdk-js'
+import {
+  NearContract,
+  NearBindgen,
+  call,
+  view,
+  near,
+  LookupMap,
+} from "near-sdk-js";
 
 @NearBindgen
-class StatusMessage extends NearContract {
-    constructor() {
-        super()
-        this.records = {}
-    }
+class UserNotes extends NearContract {
+  constructor() {
+    super();
+    this.n = new LookupMap("user_notes");
+  }
 
-    @call
-    set_status({ message }) {
-        let account_id = near.signerAccountId()
-        near.log(`${account_id} set_status with message ${message}`)
-        this.records[account_id] = message
-    }
+  @call
+  addNote({ uri }) {
+    const accountId = near.signerAccountId();
+    const notes = this.n.get(accountId);
+    this.n.set(accountId, [uri, ...notes]);
+  }
 
-    @view
-    get_status({ account_id }) {
-        near.log(`get_status for account_id ${account_id}`)
-        return this.records[account_id] || null
-    }
+  @view
+  getAllNotes({ accountId }) {
+    return this.n.get(accountId);
+  }
 }
-
