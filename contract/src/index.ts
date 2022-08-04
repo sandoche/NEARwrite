048a586dcc7f8fb13dfd2c56e452a7dd8f58a9f3
Sandoche Adittane <sandoche@gmail.com>
2022-08-04 11:28:22 +0200

    Code moved to ts

---
+++ contract/src/index.ts
@@ -0,0 +1,39 @@
+import {
+  NearContract,
+  NearBindgen,
+  call,
+  view,
+  near,
+  LookupMap,
+} from "near-sdk-js";
+import assert from "./utilities/assert";
+
+@NearBindgen
+class UserNotes extends NearContract {
+  notes: LookupMap<string, string[]>;
+
+  constructor() {
+    super();
+    this.notes = new LookupMap("user_notes");
+  }
+
+  deserialize() {
+    super.deserialize();
+    this.notes = Object.assign(new LookupMap("user_notes"), this.notes);
+  }
+
+  @call
+  addNote({ ipfsHash }) {
+    assert(ipfsHash.length === 46, "Invalid ipfsHash");
+    const accountId = near.signerAccountId();
+    near.log(`Adding the ipfsHash ${ipfsHash} to ${accountId}`);
+    const notes = this.notes.get(accountId) || [];
+    this.notes.set(accountId, [ipfsHash, ...notes]);
+  }
+
+  @view
+  getAllNotes({ accountId }) {
+    near.log(`Getting all the notes from ${accountId}`);
+    return this.notes.get(accountId);
+  }
+}
