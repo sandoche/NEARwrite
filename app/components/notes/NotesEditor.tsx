40af271804f7599f837c9079405be4c7580a182d
Sandoche Adittane <sandoche@gmail.com>
2022-08-05 16:21:26 +0200

    Work in progress contracts calls

--- app/components/notes/NotesEditor.tsx
+++ app/components/notes/NotesEditor.tsx
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import { Button, Text } from '@mantine/core';
 import dynamic from 'next/dynamic';
 
+import { CONTRACT_ID } from '@/constants';
 import en from '@/locales/en.json';
+import useContract from '@/modules/near-api-react/hooks/useContract';
 import addNoteToIpfs from '@/services/ipfs';
 
 import '@uiw/react-markdown-editor/markdown-editor.css';
@@ -13,11 +15,21 @@ const MDEditor = dynamic(() => import('@uiw/react-markdown-editor').then((mod) =
 const NotesEditor = () => {
   const [note, setNote] = useState(`# Note of  ${new Date().toDateString()}
   
+
 What's up today?`);
 
+  const contract: any = useContract({
+    contractId: CONTRACT_ID || '',
+    contractMethods: {
+      changeMethods: ['addNote'],
+      viewMethods: [],
+    },
+  });
+
   const saveNote = async () => {
-    const cid = await addNoteToIpfs(note);
-    console.info(cid);
+    // const cid = await addNoteToIpfs(note);
+    // console.info(cid);
+    await contract?.addNote({ note: 'Qmb6PeTzK6iKU4cErYVYdcSYjh5SoeuaJpuf9gnBZNsgzj' });
   };
 
   return (
